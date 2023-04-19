60907ca22102fb4a3272eeeaced5b8e6f9e7069b
estebanmont <88239099+estebanmont@users.noreply.github.com>
2023-04-19 12:03:08 -0500

    Intento Parallax

---
+++ Assets/Juanes/Parallax.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    [SerializeField] private Vector2 velocidadMovimiento;
+    private Vector2 offeset;
+    private Material material;
+
+    private void Awake()
+    {
+        material = GetComponent<SpriteRenderer>().material;
+    }
+
+    private void update()
+    {
+        offeset = velocidadMovimiento * Time.deltaTime;
+        material.mainTextureOffset += offeset;
+    }
+}
