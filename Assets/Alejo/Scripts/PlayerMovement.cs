dcd9cc0868a549be619063f3ad5c7c373c02a643
Nicolás <skarddy@gmail.com>
2023-04-19 23:52:14 -0500

    Jump animation fixed

--- Assets/Alejo/Scripts/PlayerMovement.cs
+++ Assets/Alejo/Scripts/PlayerMovement.cs
@@ -7,15 +7,17 @@ using UnityEngine.Animations;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private Vector2 jumpForce = new Vector2(0,5);
+    [SerializeField] private Vector2 jumpForce = new Vector2(0, 5);
     private Rigidbody2D m_Rigidbody;
     [SerializeField] private bool isGrounded = false;
     [SerializeField] private Animator playerAnim;
-    
-    
+    private bool isJumping = false;
+
+
     private void Awake()
     {
         m_Rigidbody = GetComponent<Rigidbody2D>();
+        playerAnim = GetComponent<Animator>();
     }
     void Update()
     {
@@ -27,22 +29,29 @@ public class PlayerMovement : MonoBehaviour
     {
         if (!Input.GetButtonDown("Jump") || !isGrounded) return;
         m_Rigidbody.AddForce(jumpForce, ForceMode2D.Impulse);
-        //
-        Debug.Log("Salto");
         isGrounded = false;
+        // Activa la variable booleana de salto
+        isJumping = true;
+        playerAnim.SetBool("isJumping", true);
     }
 
-    private void Attack() {
-        if (Input.GetButtonDown("Fire1")) {
+    private void Attack()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
             Debug.Log("ataca");
         }
     }
-    
+
     private void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
+
+            // Desactiva la variable booleana de salto
+            isJumping = false;
+            playerAnim.SetBool("isJumping", false);
         }
     }
 }
